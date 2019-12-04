f4c94418fee5c5029bc87b3dd40ab5cde1ebcb67
JTLiner925 <55215116+JTLiner925@users.noreply.github.com>
2019-12-04 16:04:26 -0600

    shopping list

---
+++ shopping-list/index.js
@@ -0,0 +1,30 @@
+'use strict';
+/* addItem - put text in entry and submit 
+"add item button" - populate under other items.*/
+$(function() {
+  $('#js-shopping-list-form').submit( event => {
+    event.preventDefault();
+    const newItem = $('#shopping-list-entry').val();
+    $('#shopping-list-entry').val('');
+    console.log(newItem);
+    $('.shopping-list').append(`
+       <li>
+       <span class="shopping-item">${newItem}</span>
+       <div class="shopping-item-controls">
+         <button class="shopping-item-toggle">
+           <span class="button-label">check</span>
+         </button>
+         <button class="shopping-item-delete">
+           <span class="button-label">delete</span>
+         </button>
+       </div>
+     </li>
+       `)
+  });
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) { 
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) { 
+    $(this).closest('li').remove()
+  });
+})
